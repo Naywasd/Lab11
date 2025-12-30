f2f35a46e4da50be3ece9d23149309261cee4afb
Naywasd <sermsub2549@gmail.com>
2025-12-30 13:24:47 +0700

    Implement future prediction feature in lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,22 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(0));
+
+    cout << "Press Enter 3 times to reveal your future.\n";
+
+    for (int i = 0; i < 3; i++) {
+        cin.get();
+    }
+
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+
+    int r = rand() % 9;
+
+    cout << "\nYou will get " << grades[r] << " in this 261102.";
+
+    return 0;
+}
